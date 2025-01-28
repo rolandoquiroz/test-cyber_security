6f2aa36bbc1534652f160a9633e95c6585fc1e66
rolandoquiroz <rolandoquiroz@gmail.com>
2025-01-28 10:56:49 -0500

    Add a Bash script that decode XOR WebSphere

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -5,6 +5,12 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-encoded_input=${1#"{xor}"}
+encoded=${1#"{xor}"}
 
-echo "$encoded_input" | base64 -d | perl -pe 's/(.)/chr(ord($1) ^ 0x5F)/ge'
+python3 -c "
+import base64, sys
+encoded = \"$encoded\"
+decoded = base64.b64decode(encoded).decode('latin1')
+result = ''.join(chr(ord(c) ^ 0x5F) for c in decoded)
+print(result)
+"
