597db6a54532e06c5d75fcf80e52e9c6528dae82
rolandoquiroz <rolandoquiroz@gmail.com>
2025-03-01 19:21:23 -0500

    Add a script to set up basic iptables firewall rules that block all incoming traffic except SSH

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-iptables -F; iptables -P INPUT DROP
-iptables -A INPUT -p tcp --dport ssh -j ACCEPT
+iptables -F;
+iptables -P INPUT DROP && iptables -A INPUT -p tcp --dport ssh -j ACCEPT && iptables -A INPUT -j DROP
