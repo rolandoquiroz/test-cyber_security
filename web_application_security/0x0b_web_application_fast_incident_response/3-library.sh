9530d53c5e7a9d3babd3fa1fd645a088b4ede702
rolandoquiroz <rolandoquiroz@gmail.com>
2025-04-06 23:57:01 -0500

    Identify the Library Used by the Attacker

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk '{ip=$1; req=$6} NR==1{max_ip=ip; max_req=req} {ip_count[ip]++; if(ip_count[ip]>ip_count[max_ip]) max_ip=ip; ip_req[ip,req]++} END{for(i in ip_req) {split(i,a,SUBSEP); if(a[1]==max_ip && ip_req[i]>max) {max=ip_req[i]; res=a[2]}}} END{print res}' logs.txt
+awk '{count[$1]++} END {for (ip in count) print count[ip], ip}' logs.txt | sort -nr | head -n1 | awk '{print $2}' | xargs -I{} awk -v ip="{}" '$1 == ip' logs.txt | awk -F'"' '{count[$6]++} END {for (req in count) print count[req], req}' | sort -nr | head -n1 | awk '{print $2}' | tr -d ' '
