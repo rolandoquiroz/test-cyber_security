8f5de38c5591d8a7ad5d6fbfda9d97ba5886653f
rolandoquiroz <rolandoquiroz@gmail.com>
2025-04-13 21:32:56 -0500

    Add a script that shows the name of the compromised account

--- web_application_security/0x0c_web_application_foresics/2-accounts.sh
+++ web_application_security/0x0c_web_application_foresics/2-accounts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 1000 auth.log | grep -i "failed" | awk '{print $11}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+tail -n 1000 auth.log | grep -E "root" | sort | uniq -c | sort -nr | head -n 1 | awk '{print $12}'
