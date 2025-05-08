c693638f612fc3bd5ff3e6729c926f42506a6c74
rolandoquiroz <rolandoquiroz@gmail.com>
2025-05-08 11:18:24 -0500

    Add a Ruby function merge_json_files that merges JSON objects from one file into another.

--- scripting_cyber/0x00-ruby_scripting/4-write_file.rb
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -5,6 +5,7 @@ def merge_json_files(file1_path, file2_path)
   data1 = JSON.parse(File.read(file1_path))
   data2 = JSON.parse(File.read(file2_path))
 
+  # Order matters
   merged_data = data2 + data1
 
   File.write(file2_path, JSON.pretty_generate(merged_data))
