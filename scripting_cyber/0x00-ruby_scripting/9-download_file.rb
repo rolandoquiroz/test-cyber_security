c132be22cf3392aabcd2a7a5a2b07a0a7264cf33
rolandoquiroz <rolandoquiroz@gmail.com>
2025-05-09 10:36:10 -0500

    Add a Ruby script that accept two args the URL and the Path to download a file from a given URL and saves it locally using open-uri, uri, fileutils.

--- scripting_cyber/0x00-ruby_scripting/9-download_file.rb
+++ scripting_cyber/0x00-ruby_scripting/9-download_file.rb
@@ -1,1 +1,16 @@
 #!/usr/bin/env ruby
+require 'open-uri'
+require 'fileutils'
+
+if ARGV.length != 2
+  puts "Usage: #{$PROGRAM_NAME} URL LOCAL_FILE_PATH"
+  exit
+end
+
+url, path = ARGV
+puts "Downloading file from #{url}..."
+
+FileUtils.mkdir_p(File.dirname(path))
+File.write(path, URI.open(url).read, mode: 'wb')
+
+puts "File downloaded and saved to #{path}."
