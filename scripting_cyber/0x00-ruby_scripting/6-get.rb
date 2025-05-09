3684ab9a5b41689e3f0a9c5cdeb8df5cbfd463f0
rolandoquiroz <rolandoquiroz@gmail.com>
2025-05-09 07:10:39 -0500

    Add a Ruby function get_request(url) that performs an HTTP GET request to the specified url

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -1,1 +1,11 @@
 #!/usr/bin/env ruby
+require 'net/http'
+require 'json'
+
+def get_request(url)
+  uri = URI(url)
+  res = Net::HTTP.get_response(uri)
+
+  puts "Response status: #{res.code} #{res.message}"
+  puts "Response body:\n#{JSON.pretty_generate(JSON.parse(res.body))}"
+end
