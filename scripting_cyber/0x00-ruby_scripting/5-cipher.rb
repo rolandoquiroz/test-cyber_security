e50131b1f7590fdc03db802037f8b51ed5594185
rolandoquiroz <rolandoquiroz@gmail.com>
2025-05-09 06:34:06 -0500

    Add a implementation of Caesar Cipher Encryption and Decryption using Classes

--- scripting_cyber/0x00-ruby_scripting/5-cipher.rb
+++ scripting_cyber/0x00-ruby_scripting/5-cipher.rb
@@ -1,1 +1,48 @@
 #!/usr/bin/env ruby
+class CaesarCipher
+  @@UPPERCASE_LETTERS = ("A".."Z").to_a
+  @@LOWERCASE_LETTERS = ("a".."z").to_a
+
+  attr_accessor :shift
+
+  def initialize(shift)
+    @shift = shift
+  end
+
+  def encrypt(message)
+    encrypted = ""
+    message.split("").to_a.each do |x|
+      if @@UPPERCASE_LETTERS.include? x
+        encrypted += @@UPPERCASE_LETTERS[ (@@UPPERCASE_LETTERS.index(x) + @shift) % 26]
+      elsif @@LOWERCASE_LETTERS.include? x
+        encrypted += @@LOWERCASE_LETTERS[ (@@LOWERCASE_LETTERS.index(x) + @shift) % 26]
+      else
+        encrypted += x
+      end
+    end
+    return encrypted
+  end
+
+  def decrypt(message)
+    decrypted = ""
+    message.split("").to_a.each do |x|
+      if @@UPPERCASE_LETTERS.include? x
+        decrypted += @@UPPERCASE_LETTERS[ (@@UPPERCASE_LETTERS.index(x) - @shift) % 26]
+      elsif @@LOWERCASE_LETTERS.include? x
+        decrypted += @@LOWERCASE_LETTERS[ (@@LOWERCASE_LETTERS.index(x) - @shift) % 26]
+      else
+        decrypted += x
+      end
+    end
+    return decrypted
+  end
+
+  private
+
+  def cipher(message, shift)
+    encrypted = encrypt(message)
+    decrypted = decrypt(encrypted)
+    puts "Encrypted message: #{encrypted}"
+    puts "Decrypted message: #{decrypted}"
+  end
+end
