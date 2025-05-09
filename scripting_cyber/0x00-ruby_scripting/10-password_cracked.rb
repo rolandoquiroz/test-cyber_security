ca03eed4a444e06d3381d993453f8b0295eb7304
rolandoquiroz <rolandoquiroz@gmail.com>
2025-05-09 11:16:03 -0500

    Add a Ruby script that accepting two args hashed_password dictionary_file to perform a dictionary attack to crack a hashed password.

--- scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
+++ scripting_cyber/0x00-ruby_scripting/10-password_cracked.rb
@@ -4,10 +4,12 @@ require 'digest'
 abort("Usage: #{$0} HASHED_PASSWORD DICTIONARY_FILE") if ARGV.size != 2
 
 hashed_password, dictionary_file = ARGV
-File.foreach(dictionary_file) { |word|
-  if Digest::SHA256.hexdigest(word.chomp) == hashed_password
-    abort("Password found: #{word.chomp}")
+File.foreach(dictionary_file) do |word|
+  word.chomp!
+  if Digest::SHA256.hexdigest(word) == hashed_password
+    puts "Password found: #{word}"
+    exit
   end
-}
+end
 
 puts "Password not found in dictionary."
